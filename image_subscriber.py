952cd545c60c4f52a7870c92c09419558d4818d8
Alex Lee <alexleegk@gmail.com>
2015-11-06 16:12:58 -0800

    Add code for getting images and moving a camera in ROS gazebo.

---
+++ image_subscriber.py
@@ -0,0 +1,134 @@
+from __future__ import division
+
+import argparse
+import numpy as np
+import rospy
+import sensor_msgs
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import h5py
+import util
+import IPython as ipy
+
+class ImageSubscriberAndRandomController:
+    def __init__(self, **kwargs):
+        self.model_name = kwargs['model_name']
+        self.num_steps = kwargs['num_steps']
+        self.vel_max = np.asarray(kwargs['vel_max'])
+        self.pos_min = np.asarray(kwargs['pos_min'])
+        self.pos_max = np.asarray(kwargs['pos_max'])
+        self.rescale_factor = kwargs['rescale_factor']
+        self.visualize = kwargs['visualize']
+        
+        if kwargs['output'] is not None:
+            self.f = h5py.File(kwargs['output'], "a")
+        else:
+            self.f = None
+        self.step = 0
+
+        if self.visualize:
+            cv2.namedWindow("Image window", 1)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber(self.model_name + '/rgb/image_raw', sensor_msgs.msg.Image, self.callback)
+        self.image = None
+        self.prev_image = None
+        self.vel = None
+        self.prev_vel = None
+    
+    def callback(self, data):
+        # get image
+        try:
+            bgr_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+        except CvBridgeError, e:
+            print e
+            self.image_sub.unregister()
+            print "Error in receiving or converting the image, shutting down"
+            rospy.signal_shutdown("Error, shutting down")
+            return
+        height, width = self.image.shape[:2]
+        self.image = cv2.resize(self.image, (int(width/self.rescale_factor), int(height/self.rescale_factor)))
+
+        if self.step != 0 and np.any(self.prev_vel) and np.allclose(self.prev_image, self.image): # non-zero vel and image hasn't changed
+            return
+
+        # generate and apply action
+        self.vel = (2*np.random.random(3) - 1) * self.vel_max
+        pose = util.get_model_pose(self.model_name)
+        self.pos = np.asarray([pose.position.x, pose.position.y, pose.position.z])
+        pos_next = np.clip(self.pos + self.vel, self.pos_min, self.pos_max)
+        self.vel = pos_next - self.pos # recompute vel because of clipping
+        pose.position.x, pose.position.y, pose.position.z = pos_next
+        util.set_model_pose(self.model_name, pose)
+
+        # visualization
+        if self.visualize:
+            cv2.imshow("Image window", self.image)
+            cv2.waitKey(1)
+
+        # save data
+        if self.f is not None:
+            image_std = util.standarize(self.image)
+            data_keys = ["image_curr", "image_next", "image_diff", "vel", "pos"]
+            image_shape = (self.num_steps, ) + image_std.T.shape
+            data_shapes = [image_shape,  image_shape, image_shape, (self.num_steps,  (self.vel_max != 0).sum()), (self.num_steps,  3)]
+            for data_key, data_shape in zip(data_keys, data_shapes):
+                if data_key in self.f:
+                    if self.f[data_key].shape != data_shape:
+                        self.image_sub.unregister()
+                        print "Error, file with different structure exists, shutting down"
+                        rospy.signal_shutdown("Error, shutting down")
+                        return
+                else:
+                    self.f.create_dataset(data_key, data_shape)
+            if self.step != 0:
+                prev_image_std = util.standarize(self.prev_image)
+                self.f["image_next"][self.step-1] = image_std.T
+                self.f["image_diff"][self.step-1] = image_std.T - prev_image_std.T
+            if self.step != self.num_steps:
+                self.f["image_curr"][self.step] = image_std.T
+                self.f["vel"][self.step] = self.vel[self.vel_max != 0] # exclude axes with fixed position
+                self.f["pos"][self.step] = self.pos
+        
+        if self.step == self.num_steps:
+            self.image_sub.unregister()
+            print "Collected all data, shutting down"
+            rospy.signal_shutdown("Collected all data, shutting down")
+            return
+        
+        self.prev_image = self.image
+        self.prev_vel = self.vel
+        self.step += 1
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', '-o', type=str, default=None)
+    parser.add_argument('--model_name', type=str, default='asus_camera')
+    parser.add_argument('--num_steps', '-n', type=int, default=100)
+    parser.add_argument('--vel_max', type=float, nargs=3, default=[.1, 0, .1], metavar=tuple([xyz + '_vel_max' for xyz in 'xyz']))
+    parser.add_argument('--pos_min', type=float, nargs=3, default=[-5, 6, .5], metavar=tuple([xyz + '_pos_min' for xyz in 'xyz']))
+    parser.add_argument('--pos_max', type=float, nargs=3, default=[5, 6, 5.5], metavar=tuple([xyz + '_pos_max' for xyz in 'xyz']))
+    parser.add_argument('--rescale_factor', '-r', type=float, default=8, metavar='r', help='rescale image by 1/r')
+    parser.add_argument('--visualize', '-v', type=int, default=1)
+    
+    args = parser.parse_args()
+    
+    rospy.init_node('image_subscriber', anonymous=True, log_level=rospy.INFO)
+
+    pos0 = (np.asarray(args.pos_min) + np.asarray(args.pos_max))/2
+    util.set_model_pose(args.model_name, util.create_pose(pos0, 0,0,np.pi/2))
+    pose = util.get_model_pose(args.model_name)
+    assert pose.position.x == pos0[0]
+    assert pose.position.y == pos0[1]
+    assert pose.position.z == pos0[2]
+    
+    ImageSubscriberAndRandomController(**vars(args))
+    
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print "Shutting down"
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ util.py
@@ -0,0 +1,143 @@
+from __future__ import division
+
+import numpy as np
+import cv2
+import rospy
+import tf
+import geometry_msgs
+import gazebo_msgs.srv
+
+def standarize(data, in_min=0, in_max=255, out_min=-1, out_max=1):
+    assert in_min <= data.min() <= data.max() <= in_max
+    return (data.astype(float) - in_min) * (out_max - out_min)/(in_max - in_min) + out_min
+
+def destandarize(data, in_min=-1, in_max=1, out_min=0, out_max=255):
+    return standarize(data, in_min=in_min, in_max=in_max, out_min=out_min, out_max=out_max)
+
+def linspace2d(start,end,n):
+    cols = [np.linspace(s,e,n) for (s,e) in zip(start,end)]
+    return np.array(cols).T
+
+def upsample_waypoints(waypoints, max_dist):
+    upsampled_waypoints = []    
+    for wp0, wp1 in zip(waypoints[:-1], waypoints[1:]):
+        dist = np.linalg.norm(np.asarray(wp1) - np.asarray(wp0))
+        num = np.ceil(dist/max_dist)
+        upsampled_waypoints.append(linspace2d(wp0, wp1, num))
+    return np.concatenate(upsampled_waypoints)
+
+def create_pose(xyz, roll, pitch, yaw):
+    pose = geometry_msgs.msg.Pose()
+    pose.position.x = xyz[0]
+    pose.position.y = xyz[1]
+    pose.position.z = xyz[2]
+    quat = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+    pose.orientation.x = quat[0]
+    pose.orientation.y = quat[1]
+    pose.orientation.z = quat[2]
+    pose.orientation.w = quat[3]
+    return pose
+
+def create_pose_msg(xyz, roll, pitch, yaw, frame_id):
+    pose_stamped = geometry_msgs.msg.PoseStamped()
+    pose_stamped.pose = create_pose(xyz, roll, pitch, yaw)
+    pose_stamped.header.frame_id = "/arm_link_0"
+    pose_stamped.header.stamp = rospy.Time.now()
+    return pose_stamped
+
+def get_model_pose(model_name, relative_entity_name='world'):
+    rospy.wait_for_service('gazebo/get_model_state')
+    try:
+        get_model_state = rospy.ServiceProxy('gazebo/get_model_state', gazebo_msgs.srv.GetModelState)
+        res = get_model_state(model_name, relative_entity_name)
+        return res.pose
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def set_model_pose(model_name, model_pose, relative_entity_name='world'):
+    rospy.wait_for_service('gazebo/set_model_state')
+    try:
+        set_model_state = rospy.ServiceProxy('gazebo/set_model_state', gazebo_msgs.srv.SetModelState)
+        model_state = gazebo_msgs.msg.ModelState()
+        model_state.model_name = model_name
+        model_state.pose = model_pose
+        model_state.reference_frame = relative_entity_name
+        set_model_state(model_state)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def arrowed_line(img, pt1, pt2, color, thickness=1, shift=0, tip_length=0.1):
+    # adapted from http://mlikihazar.blogspot.com.au/2013/02/draw-arrow-opencv.html
+    if img.ndim == 1:
+        img = np.repeat(img[:, :, None], 3, axis=2)
+    pt1 = np.asarray(pt1, dtype=int)
+    pt2 = np.asarray(pt2, dtype=int)
+    color = tuple(color)
+    # draw arrow tail
+    cv2.line(img, tuple(pt1), tuple(pt2), color, thickness=thickness, shift=shift)
+    # calc angle of the arrow 
+    angle = np.arctan2(pt1[1]-pt2[1], pt1[0]-pt2[0])
+    # starting point of first line of arrow head 
+    pt = (int(pt2[0] + tip_length * np.cos(angle + np.pi/4)),
+          int(pt2[1] + tip_length * np.sin(angle + np.pi/4)))
+    # draw first half of arrow head
+    cv2.line(img, tuple(pt), tuple(pt2), color, thickness=thickness, shift=shift)
+    # starting point of second line of arrow head 
+    pt = (int(pt2[0] + tip_length * np.cos(angle - np.pi/4)),
+          int(pt2[1] + tip_length * np.sin(angle - np.pi/4)))
+    # draw second half of arrow head
+    cv2.line(img, tuple(pt), tuple(pt2), color, thickness=thickness, shift=shift)
+
+def resize_from_scale(image, rescale_factor):
+    h, w = image.shape[:2]
+    image = cv2.resize(image, (int(w*rescale_factor), int(h*rescale_factor)), interpolation=cv2.INTER_NEAREST)
+    return image
+
+def resize_from_height(image, height, ret_factor=False):
+    h, w = image.shape[:2]
+    rescale_factor = height / float(h)
+    image = resize_from_scale(image, rescale_factor)
+    if ret_factor:
+        return (image, rescale_factor)
+    return image
+
+def create_vis_image(image_curr_data, vel_data, image_diff_data, height=480, draw_vel=True, rescale_vel=10):
+    image_curr_std = image_curr_data.T
+    image_curr = destandarize(image_curr_std).astype(np.uint8)
+
+    image_diff_std = image_diff_data.T
+    image_diff = destandarize(image_diff_std, in_min=-2, in_max=2, out_min=0, out_max=255).astype(np.uint8)
+
+    image_next_std = np.clip(image_curr_std + image_diff_std, -1, 1)
+    image_next = destandarize(image_next_std).astype(np.uint8)
+    
+    images, rescale_factors = zip(*[resize_from_height(image, height, ret_factor=True) for image in [image_curr, image_diff, image_next]])
+    # change from grayscale to bgr format
+    images = [np.repeat(image[:, :, None], 3, axis=2) if image.ndim == 2 else image for image in images]
+
+    if draw_vel:
+        h, w = images[0].shape[:2]
+        # draw coordinate system
+        arrowed_line(images[0], 
+                     (w/2, h/2), 
+                     (w/2 + rescale_factors[0], h - h/2), 
+                     [0, 0, 255],
+                     thickness=2,
+                     tip_length=5)
+        arrowed_line(images[0], 
+                     (w/2, h/2), 
+                     (w/2, h - (h/2 + rescale_factors[0])), 
+                     [0, 255, 0],
+                     thickness=2,
+                     tip_length=5)
+        # draw rescaled velocity
+        arrowed_line(images[0], 
+                     (w/2, h/2), 
+                     (w/2 + vel_data[0] * rescale_vel * rescale_factors[0], 
+                      h - (h/2 + vel_data[1] * rescale_vel * rescale_factors[0])), 
+                     [255, 0, 0],
+                     thickness=5,
+                     tip_length=15)
+    
+    vis_image = np.concatenate(images, axis=1)
+    return vis_image
