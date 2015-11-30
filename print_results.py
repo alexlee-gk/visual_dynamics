1ac629ac41ac74ce0fe4e72aae9672512f903b41
Alex Lee <alexleegk@gmail.com>
2015-11-30 12:19:43 -0800

    Handle nested groups in print_results.py

--- print_results.py
+++ print_results.py
@@ -6,21 +6,30 @@ import h5py
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('results_hdf5_fname', type=str)
-    parser.add_argument('dset_keys', nargs='*', type=str, default=[])
+    parser.add_argument('group_or_dset_keys', nargs='*', type=str, default=[])
 
     args = parser.parse_args()
 
     f = h5py.File(args.results_hdf5_fname, 'r')
-#     import IPython as ipy; ipy.embed()
-    for group_key, group in f.items():
-        print group_key
-        if args.dset_keys:
-            for dset_key in args.dset_keys:
-                if dset_key in group:
-                    print '\t' + dset_key + ':', group[dset_key][()]
+
+    def print_group_or_dset(group_or_dset_key, group_or_dset, level=1):
+        if type(group_or_dset) == h5py.Group:
+            group_key, group = group_or_dset_key, group_or_dset
+            print '\t'*level + group_key + ':'
+            for key, value in group.items():
+                print_group_or_dset(key, value, level=level+1)
+        elif type(group_or_dset) == h5py.Dataset:
+            dset_key, dset = group_or_dset_key, group_or_dset
+            print '\t'*level + dset_key + ':', dset[()]
         else:
-            for dset_key, dset in group.items():
-                print '\t' + dset_key + ':', dset[()]
+            raise
+
+    for group_key, group in f.items():
+        print group_key +  ':'
+        for group_or_dset_key, group_or_dset in group.items():
+            if args.group_or_dset_keys and group_or_dset_key not in args.group_or_dset_keys:
+                continue
+            print_group_or_dset(group_or_dset_key, group_or_dset)
 
 if __name__ == "__main__":
     main()
