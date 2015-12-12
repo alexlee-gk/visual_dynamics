bc730a650ef405452a6f04fd0b322d1ca55ff371
Alex Lee <alexleegk@gmail.com>
2015-12-11 21:36:14 -0800

    Bug fixes in python bilinear layer and add more tests.

--- layers/bilinear_layer.py
+++ layers/bilinear_layer.py
@@ -36,7 +36,7 @@ class BilinearLayer(caffe.Layer):
 
         self.outer_yu = np.empty((N, I*J)) # (y[:, :, None] * u[:, None, :]).reshape((N, I*J))
         for n in range(N):
-            self.outer_yu[n, :] = y[n, :][:, None].dot(u[n, :][:, None].T).reshape((N, I*J))
+            self.outer_yu[n, :] = y[n, :][:, None].dot(u[n, :][:, None].T).reshape((1, I*J))
 
         self.blobs[0].reshape(K, I*J)
         y_dot = self.outer_yu.dot(self.blobs[0].data.T) + u.dot(self.blobs[1].data.T)
@@ -52,15 +52,16 @@ class BilinearLayer(caffe.Layer):
         if self.param_propagate_down:
             # Gradient with respect to first weights
             self.blobs[0].reshape(K, I*J)
-            self.blobs[0].diff[...] = top[0].diff.T.dot(self.outer_yu)
+            self.blobs[0].diff[...] += top[0].diff.T.dot(self.outer_yu)
             self.blobs[0].reshape(K, I, J)
 
             # Gradient with respect to second weights
-            self.blobs[1].diff[...] = top[0].diff.T.dot(u)
+            self.blobs[1].diff[...] += top[0].diff.T.dot(u)
 
         if propagate_down[0]:
             # Gradient with respect to y
             # bottom[0].diff[...] = np.einsum("nk,kij,nj->ni", top[0].diff, self.blobs[0].data, u)
+            bottom[0].diff[...] *= 0
             for n in range(N):
                 for k in range(K):
                     bottom[0].diff[n, :] += top[0].diff[n, k] * self.blobs[0].data[k, :, :].dot(u[n, :])
@@ -70,7 +71,7 @@ class BilinearLayer(caffe.Layer):
             # bottom[1].diff[...] = np.einsum("nk,kij,ni->nj", top[0].diff, self.blobs[0].data, y) + top[0].diff.dot(self.blobs[1].data)
             outer_dy = np.empty((N, K*I)) # (top[0].diff[:, :, None] * y[:, None, :]).reshape((N, K*I))
             for n in range(N):
-                outer_dy[n, :] = top[0].diff[n, :][:, None].dot(y[n, :][:, None].T).reshape((N, K*I))
+                outer_dy[n, :] = top[0].diff[n, :][:, None].dot(y[n, :][:, None].T).reshape((1, K*I))
             self.blobs[0].reshape(K*I, J)
             bottom[1].diff[...] = outer_dy.dot(self.blobs[0].data) + top[0].diff.dot(self.blobs[1].data)
             self.blobs[0].reshape(K, I, J)
@@ -148,7 +149,7 @@ def cpu_bilinear_net(input_shapes, hdf5_txt_fname='', batch_size=1, net_name='Cp
 
     bilinear_kwargs = dict(param=[dict(lr_mult=1, decay_mult=1), dict(lr_mult=1, decay_mult=1)],
                            bilinear_filler=dict(type='gaussian', std=0.001),
-                           linear_filler=dict(type='constant', value=0))
+                           linear_filler=dict(type='gaussian', std=0.001))
 
     n = caffe.NetSpec()
     data_kwargs = dict(name='data', ntop=3, batch_size=batch_size, source=hdf5_txt_fname)
@@ -156,14 +157,12 @@ def cpu_bilinear_net(input_shapes, hdf5_txt_fname='', batch_size=1, net_name='Cp
         data_kwargs.update(dict(include=dict(phase=phase)))
     n.image_curr, n.image_diff, n.vel = L.HDF5Data(**data_kwargs)
     u = n.vel
-    n.image_diff_pred = L.Bilinear(n.image_curr, u, **bilinear_kwargs)
-
     n.y = L.Flatten(n.image_curr)
-    n.y_diff_pred = L.Flatten(n.image_diff_pred)
+    n.y_diff_pred = L.Bilinear(n.y, u, **bilinear_kwargs)
     n.y_diff = L.Flatten(n.image_diff)
-
     n.loss = L.EuclideanLoss(n.y_diff_pred, n.y_diff, name='loss')
 
+    n.image_diff_pred = L.Reshape(n.y_diff_pred, shape=dict(dim=[batch_size] + list(image_shape)))
     n.image_next_pred = L.Eltwise(n.image_curr, n.image_diff_pred, operation=P.Eltwise.SUM)
 
     net = n.to_proto()
@@ -177,56 +176,104 @@ def main():
 
     args = parser.parse_args()
 
+    np.random.seed(0)
+
     inputs = ['image_curr', 'vel']
     input_shapes = NetFeaturePredictor.infer_input_shapes(inputs, None, args.train_hdf5_fname)
     outputs = ['y_diff_pred', 'y', 'image_next_pred']
     net = NetFeaturePredictor(bilinear_net, inputs, input_shapes, outputs)
     net_p = NetFeaturePredictor(python_bilinear_net, inputs, input_shapes, outputs)
     net_c = NetFeaturePredictor(cpu_bilinear_net, inputs, input_shapes, outputs)
-    net_p.params['y_diff_pred'][0].data[...] = net.params['bilinear_fc_outer_yu'][0].data.reshape(net_p.params['y_diff_pred'][0].data.shape)
-    net_p.params['y_diff_pred'][1].data[...] = net.params['bilinear_fc_u'][0].data
-    net_c.params['image_diff_pred'][0].data[...] = net.params['bilinear_fc_outer_yu'][0].data.reshape(net_c.params['image_diff_pred'][0].data.shape)
-    net_c.params['image_diff_pred'][1].data[...] = net.params['bilinear_fc_u'][0].data
-    assert not np.any(net.params['bilinear_fc_outer_yu'][1].data)
-    assert not np.any(net.params['bilinear_fc_u'][1].data)
+    image_shape, vel_shape = input_shapes
+    bilinear_param_shape = (np.prod(image_shape), np.prod(image_shape), np.prod(vel_shape))
+    bilinear_param = net.params['bilinear_fc_outer_yu'][0]
+    linear_param = net.params['bilinear_fc_u'][0]
+    assert np.any(bilinear_param)
+    assert np.any(linear_param)
+    def make_diff_1(net):
+        for blob in net.blobs.values():
+            blob.diff[...] *= 0
+            blob.diff[...] += 1
+        for param in net.params.values():
+            for blob in param:
+                blob.diff[...] *= 0
+                blob.diff[...] += 1
+    make_diff_1(net)
 
-    np.random.seed(0)
+    # generate inputs
     image_curr = np.random.random(input_shapes[0])*2 - 1
     vel = np.random.random(input_shapes[1])*2 - 1
+    # compute output
     forward_kwargs = dict(zip(inputs, [image_curr[None, ...], vel[None, :]]))
-
     y_diff_pred = net.forward_all(blobs=['y_diff_pred'], **forward_kwargs)['y_diff_pred']
-    y_diff_pred_p = net_p.forward_all(blobs=['y_diff_pred'], **forward_kwargs)['y_diff_pred']
-    y_diff_pred_c = net_c.forward_all(blobs=['y_diff_pred'], **forward_kwargs)['y_diff_pred']
-    print "forward python", np.linalg.norm(y_diff_pred - y_diff_pred_p)
-    print "forward cpu", np.linalg.norm(y_diff_pred - y_diff_pred_c)
-
+    # compute diffs
     backward_kwargs = dict(y_diff_pred=y_diff_pred)
-    diffs = net.backward_all(start='y_diff_pred', **backward_kwargs)
+    diffs = net.backward_all(**backward_kwargs)
     image_curr_diff, vel_diff = diffs['image_curr'], diffs['vel']
-    diffs_p = net_p.backward_all(start='y_diff_pred', **backward_kwargs)
+
+    ### python bilinear net
+    net_p.params['y_diff_pred'][0].data[...] = bilinear_param.data.reshape(bilinear_param_shape)
+    net_p.params['y_diff_pred'][1].data[...] = linear_param.data
+    make_diff_1(net_p)
+    y_diff_pred_p = net_p.forward_all(blobs=['y_diff_pred'], **forward_kwargs)['y_diff_pred']
+    print "forward python", np.linalg.norm(y_diff_pred - y_diff_pred_p)
+    diffs_p = net_p.backward_all(**backward_kwargs)
     image_curr_diff_p, vel_diff_p = diffs_p['image_curr'], diffs_p['vel']
-    diffs_c = net_c.backward_all(start='y_diff_pred', **backward_kwargs)
-    image_curr_diff_c, vel_diff_c = diffs_c['image_curr'], diffs_c['vel']
     print "backward python", np.linalg.norm(image_curr_diff - image_curr_diff_p), np.linalg.norm(vel_diff - vel_diff_p)
-    print "backward python", np.linalg.norm(net_p.params['y_diff_pred'][0].diff - net.params['bilinear_fc_outer_yu'][0].diff.reshape(net_p.params['y_diff_pred'][0].diff.shape))
-    print "backward python", np.linalg.norm(net_p.params['y_diff_pred'][1].diff - net.params['bilinear_fc_u'][0].diff)
+    print "backward python", np.linalg.norm(net_p.params['y_diff_pred'][0].diff - bilinear_param.diff.reshape(bilinear_param_shape))
+    print "backward python", np.linalg.norm(net_p.params['y_diff_pred'][1].diff - linear_param.diff)
+
+    ### cpu bilinear net
+    caffe.set_mode_cpu()
+    net_c.params['y_diff_pred'][0].data[...] = bilinear_param.data.reshape(bilinear_param_shape)
+    net_c.params['y_diff_pred'][1].data[...] = linear_param.data
+    make_diff_1(net_c)
+    y_diff_pred_c = net_c.forward_all(blobs=['y_diff_pred'], **forward_kwargs)['y_diff_pred']
+    print "forward cpu", np.linalg.norm(y_diff_pred - y_diff_pred_c)
+    diffs_c = net_c.backward_all(**backward_kwargs)
+    image_curr_diff_c, vel_diff_c = diffs_c['image_curr'], diffs_c['vel']
     print "backward cpu", np.linalg.norm(image_curr_diff - image_curr_diff_c), np.linalg.norm(vel_diff - vel_diff_c)
-    print "backward cpu", np.linalg.norm(net_c.params['image_diff_pred'][0].diff - net.params['bilinear_fc_outer_yu'][0].diff.reshape(net_c.params['image_diff_pred'][0].diff.shape))
-    print "backward cpu", np.linalg.norm(net_c.params['image_diff_pred'][1].diff - net.params['bilinear_fc_u'][0].diff)
+    print "backward cpu", np.linalg.norm(net_c.params['y_diff_pred'][0].diff - bilinear_param.diff.reshape(bilinear_param_shape))
+    print "backward cpu", np.linalg.norm(net_c.params['y_diff_pred'][1].diff - linear_param.diff)
 
-    import time
-    n_iter = 1
+    ### gpu bilinear net
+    caffe.set_mode_gpu()
+    net_c.params['y_diff_pred'][0].data[...] = bilinear_param.data.reshape(bilinear_param_shape)
+    net_c.params['y_diff_pred'][1].data[...] = linear_param.data
+    make_diff_1(net_c)
+    y_diff_pred_c = net_c.forward_all(blobs=['y_diff_pred'], **forward_kwargs)['y_diff_pred']
+    print "forward gpu", np.linalg.norm(y_diff_pred - y_diff_pred_c)
+    diffs_c = net_c.backward_all(**backward_kwargs)
+    image_curr_diff_c, vel_diff_c = diffs_c['image_curr'], diffs_c['vel']
+    print "backward gpu", np.linalg.norm(image_curr_diff - image_curr_diff_c), np.linalg.norm(vel_diff - vel_diff_c)
+    print "backward gpu", np.linalg.norm(net_c.params['y_diff_pred'][0].diff - bilinear_param.diff.reshape(bilinear_param_shape))
+    print "backward gpu", np.linalg.norm(net_c.params['y_diff_pred'][1].diff - linear_param.diff)
 
-    start_time = time.time()
-    for _ in range(n_iter):
-        net_c.forward_all(blobs=['y_diff_pred'], **forward_kwargs)
-    print "forward duration", (time.time() - start_time) / n_iter
 
-    start_time = time.time()
-    for _ in range(n_iter):
-        net_c.backward_all(start='y_diff_pred', **backward_kwargs)
-    print "backward duration", (time.time() - start_time) / n_iter
+    def time_forward(net, n_iter=100):
+        start_time = time.time()
+        for _ in range(n_iter):
+            net.forward_all(blobs=['y_diff_pred'], **forward_kwargs)
+        return (time.time() - start_time) / n_iter
+    def time_backward(net, n_iter=100):
+        start_time = time.time()
+        for _ in range(n_iter):
+            net.backward_all(**backward_kwargs)
+        return (time.time() - start_time) / n_iter
+
+    print "forward duration", time_forward(net)
+    print "backward duration", time_backward(net)
+
+    print "forward python duration", time_forward(net_p)
+    print "backward python duration", time_backward(net_p)
+
+    caffe.set_mode_cpu()
+    print "forward cpu duration", time_forward(net_c)
+    print "backward cpu duration", time_backward(net_c)
+
+    caffe.set_mode_gpu()
+    print "forward gpu duration", time_forward(net_c)
+    print "backward gpu duration", time_backward(net_c)
 
 if __name__ == "__main__":
     main()
