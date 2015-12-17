b241ca562e05884ee9890d7b65a6d88c88097809
Alex Lee <alexleegk@gmail.com>
2015-12-17 14:25:42 -0800

    Update visualize_data for image format

--- visualize_data.py
+++ visualize_data.py
@@ -10,17 +10,19 @@ def main():
     parser.add_argument('file', type=str)
     parser.add_argument('--rescale_factor', type=int, default=10)
     parser.add_argument('--draw_vel', type=int, default=0)
-    
+
     args = parser.parse_args()
 
     f = h5py.File(args.file, 'r+')
-    
+
     cv2.namedWindow("Image window", 1)
 
-    for image_curr_data, vel_data, image_diff_data, image_next_data in zip(f['image_curr'], f['vel'], f['image_diff'], f['image_next']):
+    for i, (image_curr_data, vel_data, image_diff_data, image_next_data) in enumerate(zip(f['image_curr'], f['vel'], f['image_diff'], f['image_next'])):
         try:
+            image_curr_data = (image_curr_data + 1.0) / 2.0
+            image_diff_data = image_diff_data / 2.0
             vis_image = util.create_vis_image(image_curr_data, vel_data, image_diff_data, rescale_factor=args.rescale_factor, draw_vel=args.draw_vel)
-            
+
             cv2.imshow("Image window", vis_image)
             key = cv2.waitKey(0)
             key &= 255
