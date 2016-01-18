b3f50fecbfb1ac6fe62b5c5d8d333fda7325d657
Alex Lee <alexleegk@gmail.com>
2016-01-17 17:12:21 -0800

    Add specialized servoing controller, which learns weights to weight the features

--- controller.py
+++ controller.py
@@ -23,11 +23,12 @@ class RandomController(Controller):
 
 
 class ServoingController(Controller):
-    def __init__(self, feature_predictor, alpha=1.0, vel_max=None, lambda_=0.0):
+    def __init__(self, feature_predictor, alpha=1.0, vel_max=None, lambda_=0.0, w=None):
         self.predictor = feature_predictor
         self.alpha = alpha
-        self.lambda_ = lambda_
         self.vel_max = vel_max
+        self.lambda_ = lambda_
+        self.w = w
         self._image_target = None
         self._y_target = None
 
@@ -38,8 +39,12 @@ class ServoingController(Controller):
 
             # use model to optimize for action
             J = self.predictor.jacobian_control(x, None)
+            if self.w is not None:
+                JW = J * self.w[:, None]
+            else:
+                JW = J
             try:
-                u = self.alpha * np.linalg.solve(J.T.dot(J) + self.lambda_*np.eye(J.shape[1]), J.T.dot(self.y_target - y))
+                u = self.alpha * np.linalg.solve(JW.T.dot(J) + self.lambda_*np.eye(J.shape[1]), JW.T.dot(self.y_target - y))
             except np.linalg.LinAlgError:
                 u = np.zeros(self.predictor.u_shape)
         else:
@@ -69,3 +74,16 @@ class ServoingController(Controller):
 
     def set_target_obs(self, image_target):
         self.image_target = image_target
+
+
+class SpecializedServoingController(ServoingController):
+    def __init__(self, feature_predictor, pos_image_targets, neg_image_targets, alpha=1.0, vel_max=None, lambda_=0.0):
+        from sklearn import linear_model
+        Y_train = np.r_[feature_predictor.feature_from_input(pos_image_targets),
+                        feature_predictor.feature_from_input(neg_image_targets)]
+        label_train = np.r_[np.ones(len(pos_image_targets)),
+                            -np.ones(len(neg_image_targets))]
+        regr = linear_model.LinearRegression()
+        regr.fit(Y_train, label_train)
+        w = regr.coef_
+        super(SpecializedServoingController, self).__init__(feature_predictor, alpha=alpha, vel_max=vel_max, lambda_=lambda_, w=w)
