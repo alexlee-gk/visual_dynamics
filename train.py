47d9a159e4b88974b98298bc037dad94f7a9318c
Alex Lee <alexleegk@gmail.com>
2016-03-22 13:29:35 -0700

    Change behavior of transformers in predictor config so that all transformers must be specified with optional None values to be filled in at runtime.

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument('train_data_fnames', nargs='+', type=str)
     parser.add_argument('--val_data_fname', type=str)
     parser.add_argument('--predictor_fname', '-p', type=str, default='config/predictor/predictor.yaml')
-    parser.add_argument('--solver_fname', '--sf', type=str, default=None)
+    parser.add_argument('--solver_fname', '--sf', type=str, default='config/solver/adam.yaml')
     parser.add_argument('--no_train', action='store_true')
     parser.add_argument('--visualize', '-v', type=int, default=1)
     parser.add_argument('--vis_scale', '-s', type=int, default=10, metavar='S', help='rescale image by S for visualization')
@@ -33,20 +33,32 @@ def main():
 
     # transformers
     transformers = [utils.config.from_config(transformer_config) for transformer_config in (predictor_config.get('transformers') or [])]
+    image_transformer, vel_transformer = transformers
     # image transformer
-    image_transformer = utils.transformer.ScaleOffsetTransposeTransformer(scale=2.0/255.0, offset=-1.0, transpose=(2, 0, 1))
-    if len(transformers) >= 1:
-        image_transformer = utils.transformer.CompositionTransformer([transformers[0], image_transformer])
+    image_sot_transformer = None
+    try:
+        image_transformers = image_transformer.transformers
+    except AttributeError:
+        image_transformers = [image_transformer]
+    for transformer in image_transformers:
+        if isinstance(transformer, utils.transformer.ScaleOffsetTransposeTransformer):
+            image_sot_transformer = transformer
+    if image_sot_transformer is not None:
+        if image_sot_transformer.scale is None:
+            image_sot_transformer.scale = 2.0/255.0
+        if image_sot_transformer.offset is None:
+            image_sot_transformer.offset = -1.0
+        if image_sot_transformer.transpose is None:
+            image_sot_transformer.transpose = (2, 0, 1)
     # velocity transformer
-    with utils.container.MultiDataContainer(data_fnames) as data_container:
-        vel_limits = data_container.get_info('simulator_config')['dof_vel_limits']
-    vel_min, vel_max = (np.asarray(limit) for limit in vel_limits)
-    vel_scale = utils.math_utils.divide_nonzero(2.0, vel_max - vel_min)
-    vel_offset = -vel_scale * (vel_min + vel_max) / 2.0
-    vel_transformer = utils.transformer.ScaleOffsetTransposeTransformer(vel_scale, vel_offset)
-    if len(transformers) >= 2:
-        vel_transformer = utils.transformer.CompositionTransformer([transformers[1], vel_transformer])
-    transformers = [image_transformer, vel_transformer]
+    if isinstance(vel_transformer, utils.transformer.ScaleOffsetTransposeTransformer):
+        with utils.container.MultiDataContainer(data_fnames) as data_container:
+            vel_limits = data_container.get_info('simulator_config')['dof_vel_limits']
+        vel_min, vel_max = (np.asarray(limit) for limit in vel_limits)
+        if vel_transformer.scale is None:
+            vel_transformer.scale = utils.math_utils.divide_nonzero(2.0, vel_max - vel_min)
+        if vel_transformer.offset is None:
+            vel_transformer.offset = -vel_transformer.scale * (vel_min + vel_max) / 2.0
     predictor_config['transformers'] = [transformer.get_config() for transformer in transformers]
 
     feature_predictor = utils.config.from_config(predictor_config)
@@ -64,7 +76,6 @@ def main():
                                                              batch_size=0,
                                                              shuffle=False)
         for image_curr, vel, image_next in val_data_gen:
-            print(vel)
             image_next_pred = feature_predictor.next_maps(image_curr, vel, preprocessed=True)[0]
             image_pred_error = (image_next_pred - image_next)/2.0
             vis_image, done = utils.visualization.visualize_images_callback(
