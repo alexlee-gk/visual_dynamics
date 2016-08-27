0381588ca8b1525eab0e3e7591acdf8a88d7776e
Alex Lee <alexleegk@gmail.com>
2016-08-26 17:48:07 -0700

    Update generate_data to use new api

--- generate_data.py
+++ generate_data.py
@@ -1,58 +1,94 @@
 import argparse
-import cv2
-import controller
+import numpy as np
+import yaml
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.animation as manimation
+from gui.grid_image_visualizer import GridImageVisualizer
 import utils
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('sim_fname', type=str, help='config file with simulator arguments')
+    parser.add_argument('env_fname', type=str, help='config file with environment arguments')
+    parser.add_argument('pol_fname', type=str, help='config file with policy arguments')
     parser.add_argument('--output_dir', '-o', type=str, default=None)
     parser.add_argument('--num_trajs', '-n', type=int, default=10, metavar='N', help='total number of data points is N*T')
     parser.add_argument('--num_steps', '-t', type=int, default=10, metavar='T', help='number of time steps per trajectory')
-    parser.add_argument('--visualize', '-v', type=int, default=0)
-    parser.add_argument('--vis_scale', '-s', type=int, default=1, metavar='S', help='rescale image by S for visualization')
+    parser.add_argument('--visualize', '-v', type=str, default=None)
     args = parser.parse_args()
 
-    with open(args.sim_fname) as yaml_string:
-        sim = utils.config.from_yaml(yaml_string)
+    with open(args.env_fname) as yaml_string:
+        env = utils.config.from_yaml(yaml_string)
+
+    with open(args.pol_fname) as yaml_string:
+        policy_config = yaml.load(yaml_string)
+        replace_config = {'env': env,
+                          'action_space': env.action_space,
+                          'state_space': env.state_space}
+        try:
+            replace_config['target_env'] = env.car_env
+        except AttributeError:
+            pass
+        # TODO: better way to populate config with existing instances
+        pol = utils.from_config(policy_config, replace_config=replace_config)
 
     if args.output_dir:
         container = utils.container.ImageDataContainer(args.output_dir, 'x')
-        container.reserve(['image', 'dof_val'], (args.num_trajs, args.num_steps+1))
-        container.reserve('vel', (args.num_trajs, args.num_steps))
-        container.add_info(simulator_config=sim.get_config())
+        container.reserve(env.sensor_names + ['state'], (args.num_trajs, args.num_steps + 1))
+        container.reserve('action', (args.num_trajs, args.num_steps))
+        container.add_info(environment_config=env.get_config())
+        container.add_info(policy_config=pol.get_config())
     else:
         container = None
 
-    ctrl = controller.RandomController(*sim.action_bounds)
+    record = args.visualize and args.visualize.endswith('.mp4')
+    if args.visualize:
+        fig = plt.figure(figsize=(16, 12), frameon=False, tight_layout=True)
+        gs = gridspec.GridSpec(1, 1)
+        image_visualizer = GridImageVisualizer(fig, gs[0], len(env.sensor_names))
+        plt.show(block=False)
+
+        if record:
+            FFMpegWriter = manimation.writers['ffmpeg']
+            writer = FFMpegWriter(fps=1.0 / env.dt)
+            writer.setup(fig, args.visualize, fig.dpi)
+
     done = False
     for traj_iter in range(args.num_trajs):
         print('traj_iter', traj_iter)
         try:
-            dof_val_init = sim.sample_state()
-            sim.reset(dof_val_init)
+            state = pol.reset()
+            env.reset(state)
             for step_iter in range(args.num_steps):
-                dof_val = sim.dof_values
-                image = sim.observe()
-                action = ctrl.step(image)
-                action = sim.apply_action(action)
+                state = env.get_state()
+                obs = env.observe()
+                action = pol.act(obs)
+                env.step(action)  # action is updated in-place if needed
                 if container:
-                    container.add_datum(traj_iter, step_iter, image=image, dof_val=dof_val, vel=action)
+                    container.add_datum(traj_iter, step_iter, state=state, action=action,
+                                        **dict(zip(env.sensor_names, obs)))
                     if step_iter == (args.num_steps-1):
-                        image_next = sim.observe()
-                        container.add_datum(traj_iter, step_iter+1, image=image_next, dof_val=sim.dof_values)
+                        obs_next = env.observe()
+                        container.add_datum(traj_iter, step_iter + 1, state=env.get_state(),
+                                            **dict(zip(env.sensor_names, obs_next)))
                 if args.visualize:
-                    vis_image, done = utils.visualization.visualize_images_callback(image, vis_scale=args.vis_scale, delay=0)
-                if done:
-                    break
+                    env.render()
+                    try:
+                        image_visualizer.update(obs)
+                        if record:
+                            writer.grab_frame()
+                    except:
+                        done = True
+                    if done:
+                        break
             if done:
                 break
         except KeyboardInterrupt:
             break
-    sim.stop()
-    if args.visualize:
-        cv2.destroyAllWindows()
+    env.close()
+    if record:
+        writer.finish()
     if container:
         container.close()
 
