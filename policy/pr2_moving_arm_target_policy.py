48e59eff3943639acb117e26b8686ec2d588f531
Alex Lee <alexleegk@gmail.com>
2016-09-10 19:10:07 -0700

    Real world adjustments for pr2 moving arm policy. TODO: continuously move the arm when act is not constantly called.

--- policy/pr2_moving_arm_target_policy.py
+++ policy/pr2_moving_arm_target_policy.py
@@ -19,31 +19,36 @@ class Pr2MovingArmTargetPolicy(Pr2TargetPolicy):
             lr: 'l' for left arm and 'r' for right arm
             gripper_state_space: space of the target position for the gripper tool frame
         """
-        super(Pr2MovingArmTargetPolicy, self).__init__()
+        super(Pr2MovingArmTargetPolicy, self).__init__(env, frame_id, offset)
         self.pr2 = self.env.pr2
         self.lr = lr
-        default_gripper_state_spaces = dict(r=spaces.BoxSpace(np.array([.5, -.5, .8]),
-                                                              np.array([.7, .2, 1])),
-                                            l=spaces.BoxSpace(np.array([.5, -.2, .8]),
-                                                              np.array([.7, .5, 1])))
+        default_gripper_state_spaces = dict(r=spaces.BoxSpace(np.array([.5, -.3, .8]),
+                                                              np.array([.7, .3, 1])),
+                                            l=spaces.BoxSpace(np.array([.5, -.3, .8]),
+                                                              np.array([.7, .3, 1])))
         self.gripper_state_space = gripper_state_space or default_gripper_state_spaces[self.lr]
+        target_pos = (self.gripper_state_space.low + self.gripper_state_space.high) / 2.0
+        self.start_arm_trajectory(target_pos, wait=True, speed_factor=1)
 
     def act(self, obs):
         if not self.env.pr2.is_moving():
             self.start_arm_trajectory()
-        return super(Pr2MovingArmTargetPolicy, self).act()
+        return super(Pr2MovingArmTargetPolicy, self).act(obs)
 
     def reset(self):
-        self.pr2.join_all()
-        self.start_arm_trajectory()
+        if not self.env.pr2.is_moving():
+            self.start_arm_trajectory()
         return super(Pr2MovingArmTargetPolicy, self).reset()
 
-    def start_arm_trajectory(self):
-        target_pos = self.gripper_state_space.sample().tolist()
+    def start_arm_trajectory(self, target_pos=None, wait=False, speed_factor=.1):
+        if target_pos is None:
+            target_pos = self.gripper_state_space.sample()
+        if isinstance(target_pos, np.ndarray):
+            target_pos = target_pos.tolist()
         self.pr2.update_rave()
         traj = planning.plan_up_trajectory(self.pr2.robot, self.lr, target_pos)
         bodypart2traj = {"%sarm" % self.lr: traj}
-        pr2_trajectories.follow_body_traj(self.pr2, bodypart2traj, wait=False, speed_factor=.25)
+        pr2_trajectories.follow_body_traj(self.pr2, bodypart2traj, wait=wait, speed_factor=speed_factor)
 
     def _get_config(self):
         config = super(Pr2MovingArmTargetPolicy, self)._get_config()
