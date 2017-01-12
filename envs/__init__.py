b8b892bf5457aa13cb1d0fc6d4e607c298106823
Alex Lee <alexleegk@gmail.com>
2017-01-11 16:31:06 -0800

    Add servoing_env wrapper.

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,6 +1,7 @@
 from .base import *
 from .env_spec import *
 try:
+    from .servoing_env import *
     from .panda3d_env import *
     from .car_panda3d_env import *
     from .quad_panda3d_env import *
